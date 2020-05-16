9b0778179ad700c8570cbef59a2a5b032989d381
Margaret Egner <hellogoodbyemae@gmail.com>
2020-05-16 00:17:27 -0400

    got buttons to be added on page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,45 +1,39 @@
-// Adding click event listen listener to all buttons
-$("button").on("click", function() {
-    // Grabbing and storing the data-animal property value from the button
-    var animal = $(this).attr("data-animal");
+// Topcs Variable
+var topics = ["adventure", "yoga pose", "forrest", "northern lights", "tattoos", "crystals", "guitars", "aliens", "game of thrones", "landscape", "moon", "space", "tarot", "meditation", "astronomy", "zodiac signs", "kristen wiig", "photography", "autumn", "fail", "ghost"];
 
-    // Constructing a queryURL using the animal name
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-      animal + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
+//Functions
 
-    // Performing an AJAX request with the queryURL
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    })
-      // After data comes back from the request
-      .then(function(response) {
-        console.log(queryURL);
+	function renderButtons () {
+		$(".buttons-view").empty();
+		for (var i = 0; i < topics.length; i++) {
+			var newButton = $("<button>");
+			newButton.addClass("topic btn btn-default");
+			newButton.attr("data-name", topics[i]);
+			newButton.text(topics[i]);
+			$(".buttons-view").append(newButton);
+		}
+	};
 
-        console.log(response);
-        // storing the data from the AJAX request in the results variable
-        var results = response.data;
+	$("#add-topic").on("click", function (event) {
+		event.preventDefault();
+		var topic = $("#topic-input").val().toLowerCase().trim();
+		var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
 
-        // Looping through each result item
-        for (var i = 0; i < results.length; i++) {
+		$.ajax({
+          url: queryURL,
+          method: "GET"
+        }).done(function(response) {
 
-          // Creating and storing a div tag
-          var animalDiv = $("<div>");
+	        if (response.data.length == 0) {
+	        	alert("No Gifs found for topic");
+	        }
+			else if (topics.indexOf(topic) != -1) {
+				alert("Topic already exists");
+			}
+			else {
+				topics.push(topic);
+				renderButtons();
+			}
 
-          // Creating a paragraph tag with the result item's rating
-          var p = $("<p>").text("Rating: " + results[i].rating);
-
-          // Creating and storing an image tag
-          var animalImage = $("<img>");
-          // Setting the src attribute of the image to a property pulled off the result item
-          animalImage.attr("src", results[i].images.fixed_height.url);
-
-          // Appending the paragraph and image tag to the animalDiv
-          animalDiv.append(p);
-          animalDiv.append(animalImage);
-
-          // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
-          $("#gifs-appear-here").prepend(animalDiv);
-        }
-      });
-  });
+        });    
+	});
